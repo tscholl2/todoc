6942f391cecbccf7c889849c90c15c37075b5738
Travis Scholl <traviswscholl@gmail.com>
2020-03-18 13:26:30 -0700

    passing tests

--- src/item_test.c
+++ src/item_test.c
@@ -14,19 +14,16 @@ int main()
 ================================================================================\n\
 This is a sample todo.\n\n\n\
 ";
-  FILE* f = fmemopen(s,sizeof(s),"rb");
-  // FILE* f = fopen("a.txt","rb");
-  char* b;
-  size_t size;
-  // FILE* f = open_memstream(&b,&size);
-  // fprintf(f,"%s",s);
+  FILE* f = fmemopen(NULL,5000,"rb+");
+  fprintf(f,"%s",s);
   fseek(f,0,SEEK_SET);
   Item* a = Item_read(f);
-  assert(fclose(f) == 0);
+  fclose(f);
+  char* b;
+  size_t size;
   f = open_memstream(&b,&size);
   Item_write(a,f);
   fclose(f);
-  printf("%s\n",b);
   assert(memcmp(s,b,strlen(s)) == 0);
 
   // Test compare
